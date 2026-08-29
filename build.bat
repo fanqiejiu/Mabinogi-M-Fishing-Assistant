e1441bd7daecb3a980cf5656bced73043d04bfa2
番茄啾 <fanqiejiu@users.noreply.github.com>
2026-08-30 01:33:27 +0800

    build: embed project icon in executable

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ echo Installing packaging tool...
 .venv\Scripts\python.exe -m pip install --disable-pip-version-check --index-url https://pypi.org/simple -r requirements-dev.txt || goto :error
 
 echo Building Windows executable...
-.venv\Scripts\python.exe -m PyInstaller --clean --noconfirm --windowed --onefile --name ok-MabinogiFishing --add-data "fishing_assistant\assets;fishing_assistant\assets" --collect-all pynput --collect-all mss --collect-all ok mabinogi_fishing_helper.py || goto :error
+.venv\Scripts\python.exe -m PyInstaller --clean --noconfirm --windowed --onefile --name ok-MabinogiFishing --icon "fishing_assistant\assets\tomato_fish_icon.ico" --add-data "fishing_assistant\assets;fishing_assistant\assets" --collect-all pynput --collect-all mss --collect-all ok mabinogi_fishing_helper.py || goto :error
 
 echo.
 echo Build complete: dist\ok-MabinogiFishing.exe
